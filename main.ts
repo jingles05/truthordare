a4941f33f5657f4740c9db05a9316eb5c9c8a842
Jingles05 <102975990+jingles05@users.noreply.github.com>
2022-04-06 08:41:47 -0600

    Added a B button action

--- main.ts
+++ main.ts
@@ -7,5 +7,19 @@ input.onButtonPressed(Button.A, function () {
         basic.showString("Dare")
     }
 })
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    arrow = randint(0, 4)
+    if (arrow == 0) {
+        basic.showArrow(ArrowNames.North)
+    } else if (arrow == 1) {
+        basic.showArrow(ArrowNames.East)
+    } else if (arrow == 2) {
+        basic.showArrow(ArrowNames.South)
+    } else {
+        basic.showArrow(ArrowNames.West)
+    }
+})
+let arrow = 0
 let or = 0
 basic.showString("Truth or Dare")
